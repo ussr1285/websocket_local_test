21690d081b3c6c93e29c6d1a34b365bedd687c3b
Minjae Joe <ipszone0525@gmail.com>
2024-11-19 14:42:19 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인 허용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"서버에서 받은 메시지: {data}")
